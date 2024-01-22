32aec0d2ee3852e953b307b44d0483700d53e0e4
jovemcarlosdev <carlosandre116@outlook.com>
2024-01-22 15:19:30 -0300

    Variáveis e tipagem

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Integer
+$x = 10;
+$x = 15;
+
+// String
+$example = 'x is $x';
+$exampleInterpreted = "x is $x";
+echo $exampleInterpreted;
+
+// Array
+$array = [10, 20, 30, 'carlos', true, "$x"];
+
+// Object
+$date = new DateTime();
+
+// Null
+$null = null;
