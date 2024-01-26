9f72b85ef5af27a3acf170e871146625adf927b7
jovemcarlosdev <carlosandre116@outlook.com>
2024-01-26 11:00:15 -0300

    Condicionais

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
 
-$array = [1, 2, 3, 'carlos', [10, 20, 30]];
-$b = array(1, 2, 3, 4);
+// if($condition) {
+//     ...
+// } elseif($another_condition) {
+//     ...
+// } else {
+//     ...
+// }
 
-// print_r($array);
-// var_dump($array);
 
-// each item
-    // key
-    // value
+// switch($var) {
+//     case 'valor 1':
+//         ...
+//         break;
+//     case 'valor 2':
+//     case 'valor 3':
+//         ...
+//         break;
+//     default:
+//         ...
+// }
 
-// Indexed
-$c = [
-    0 => 1,
-    23 => 2,
-    55 => 'carlos'
-];
 
-// Associative
-$d = [
-    'a' => 1,
-    'c' => 2,
-    'xyz' => 'carlos'
-];
-
-$d['novo'] = 'novo valor';
-echo $d['novo'];
+// (condition) ? do this if true : otherwise do this;
