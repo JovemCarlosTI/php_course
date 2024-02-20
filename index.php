ad7811072c6529c9b2159da457cb064fd23ffd4d
jovemcarlosdev <carlosandre116@outlook.com>
2024-02-20 10:15:14 -0300

    Break and continue

--- index.php
+++ index.php
@@ -1,57 +1,27 @@
 <?php
 
-
-
-
-$people = [
-    ['name' => 'Sarah', 'age' => 5],
-    ['name' => 'John', 'age' => 35],
-    ['name' => 'Sherlock', 'age' => 34],
-    ['name' => 'Watson', 'age' => 40],
-    ['name' => 'Rachel', 'age' => 9],
-    ['name' => 'Joseph', 'age' => 25],
-    ['name' => 'Ross', 'age' => 7],
-    ['name' => 'Monica', 'age' => 12],
-    ['name' => 'Chandler', 'age' => 15],
-    ['name' => 'Joey', 'age' => 17],
-];
-
-// // Quantidade de pessoas com mais de 18 anos de idade
-// $count_adults = 0;
-// $count_childs = 0;
-
-// foreach($people as $person) {
-//     if($person['age'] >= 18) $count_adults++;
-//     else $count_childs++;
-// }
-
-// echo "Tem $count_adults adultos e $count_childs crianças" . PHP_EOL;
-
-// // Lista de adultos entre 22 a 30
-// $adults_for_job_opening = [];
-// foreach($people as $person) {
-//     if($person['age'] >= 22 && $person['age'] <= 30) {
-//         $adults_for_job_opening[] = $person;
-//     }
-// }
-
-// print_r($adults_for_job_opening);
-
-// Mesclagem de soluções
-$count_adults = 0;
-$count_childs = 0;
-$adults_for_job_opening = [];
-foreach($people as $person) {
-    $age = $person['age'];
-    if($age >= 18) {
-        $count_adults++;
-
-        if($age >= 22 && $age <= 30) $adults_for_job_opening[] = $person;
-    } else {
-        $count_childs++;
+// break
+$names = ['John', 'Alex', 'Simon', 'Amir', 'Carlos'];
+$name_to_find = 'Alex';
+$found_name = false;
+foreach ($names as $name) {
+    if ($name === $name_to_find) {
+        $found_name = true;
+        break;
     }
 }
 
-echo "Tem $count_adults adultos e $count_childs crianças" . PHP_EOL;
-echo "Lista de possíveis candidatos para a vaga de emprego: " . PHP_EOL;
-print_r($adults_for_job_opening);
+if ($found_name) {
+    echo "$name_to_find is found" . PHP_EOL;
+} else {
+    echo "$name_to_find is not found" . PHP_EOL;
+}
+
+// continue
+$numbers = [3, 3000, 457, 786, 234];
+foreach ($numbers as $number) {
+    if ($number % 3 == 0) {
+        continue;
+    }
+    echo $number . PHP_EOL;
+}
