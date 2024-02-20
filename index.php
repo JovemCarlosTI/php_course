41899eae5240bbba83fe887ad35f9f1f66176fba
jovemcarlosdev <carlosandre116@outlook.com>
2024-02-20 10:20:51 -0300

    Loops in HTML

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
-<?php
-
-// break
-$names = ['John', 'Alex', 'Simon', 'Amir', 'Carlos'];
-$name_to_find = 'Alex';
-$found_name = false;
-foreach ($names as $name) {
-    if ($name === $name_to_find) {
-        $found_name = true;
-        break;
-    }
-}
-
-if ($found_name) {
-    echo "$name_to_find is found" . PHP_EOL;
-} else {
-    echo "$name_to_find is not found" . PHP_EOL;
-}
-
-// continue
-$numbers = [3, 3000, 457, 786, 234];
-foreach ($numbers as $number) {
-    if ($number % 3 == 0) {
-        continue;
-    }
-    echo $number . PHP_EOL;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de HTML</title>
+</head>
+<body>
+    <?php
+        $names = ['pratham', 'simon', 'amir', 'carlos', 'vitto', 'oliver'];
+    ?>
+    <h1>Names</h1>
+    <ul>
+        <?php foreach($names as $name) {
+            echo "<li>$name</li>";
+        }?>
+    </ul>
+</body>
+</html>
