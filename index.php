7a23056c219ae4a8903a49955a7d162b9a99c887
jovemcarlosdev <carlosandre116@outlook.com>
2024-02-28 16:04:23 -0300

    Project Age In Days Calculator

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 // strtotime
 // $timestamp = strtotime(datetime: '20 feb 2024');
 
-// date
-echo date(format: 'd/m/Y - l ( H:i:s T )');
+$nascimento = '19 jun 2004';
+$dias_desde_nascimento = floor((strtotime('now') - strtotime($nascimento)) / 3600 / 24);
+
+echo "Dias desde o nascimento em $nascimento: $dias_desde_nascimento dias.";
 ?>
