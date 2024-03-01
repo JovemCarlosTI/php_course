5dec5b438d489abd128374af1a88e422ea2262af
jovemcarlosdev <carlosandre116@outlook.com>
2024-03-01 09:42:25 -0300

    JSON e XML

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
-    // Read CSV
-    $file = fopen('./example.csv', 'r');
+    // Read JSON
+    $json = '{"name": "John", "age": 30, "city": "New York"}';
+    $array = json_decode($json, associative: true);
+    print_r($array);
 
-    $line = fgetcsv($file);
-    while($line) {
-        $id = $line[0];
-        $name = $line[1];
+    // Write JSON
+    $json = json_encode(["name" => "John", "age" => 30, "city" => "New York"]);
+    echo $json;
 
-        echo "ID: $id, Name: $name" . PHP_EOL;
-        $line = fgetcsv($file);
+    // Read XML
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>
+    <accounts>
+        <account>
+            <name>John</name>
+            <age>30</age>
+            <city>New York</city>
+        </account>
+        <account>
+            <name>Peter</name>
+            <age>35</age>
+            <city>Chicago</city>
+        </account>
+        </accounts>';
+
+    $dom = new DOMDocument();
+    $dom->loadXML($xml);
+    $accounts = $dom->getElementsByTagName("account");
+
+    foreach ($accounts as $account) {
+        $name = $account->getElementsByTagName("name")[0]->nodeValue;
+        $age = $account->getElementsByTagName("age")[0]->nodeValue;
+        $city = $account->getElementsByTagName("city")[0]->nodeValue;
+        echo "Nome: $name, Idade: $age, Cidade: $city" . PHP_EOL;
     }
 
-    fclose($file);
+    // Write XML
+    $accounts = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><accounts></accounts>');
+    $accounts->addChild("account", "John")->addAttribute('type', 'admin');
+    $accounts->addChild("account", "Peter")->addAttribute('type', 'user');
 
-    // Write CSV
-    $file = fopen('./example.csv', 'a');
-
-    fputcsv($file, ['4', 'Pinterest']);
-
-    fclose($file);
+    $accounts->asXML("accounts.xml");
 ?>
