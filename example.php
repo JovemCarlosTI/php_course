6640126602f9c156eaf91db326dc98bf94784c2e
jovemcarlosdev <carlosandre116@outlook.com>
2024-03-01 10:18:41 -0300

    Dynamic website with template

--- example.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "Hello from example.php!" . PHP_EOL;
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // include "example.php";
-    require "example.php";
-    echo "Hello from index.php!" . PHP_EOL;
-    require "example.php";
-
-    // include_once "example.php";
-    // require_once "example.php";
-?>
